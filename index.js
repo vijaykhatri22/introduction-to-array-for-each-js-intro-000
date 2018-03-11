84dd1893029b5a76d69d3ac11269a92aecbe774c
vijay khatri <vijaykhatri22@gmail.com>
2018-03-11 09:56:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function doToElementsInArray(array,callback){
     array.forEach(callback);
 }
+
+function changeCompletely(element,index,array){
+    array[index]= Math.floor(Math.random() * 100 + 2  ).toString() + `${array[index]}s!!`;
+}
